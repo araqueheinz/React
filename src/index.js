44d97e8c7009fa26b63e28d741c604a2a95e157f
Heinz Araque <araqueheinz@gmail.com>
2019-07-29 11:13:51 -0400

    React Inital Setup, Hello Wolrd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+// Import the React and ReactDom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create react component (Function or Class)
+const App = () => {
+  return <div>Hello World!</div>;
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Take React Component and show it to the screen
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
