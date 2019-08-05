91f5b17a20691f59e8cdceebd6e685f915c9f501
Heinz Araque <araqueheinz@gmail.com>
2019-08-04 20:29:46 -0400

    Intro to Redux initial setup

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// Import React and ReactDOM libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App'
-
-
-// Take the App component and show it on the screen
-ReactDOM.render(<App />, document.querySelector('#root'));
